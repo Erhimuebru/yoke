7fde5ae4d34c75cb8de259e80080c280a0d32a11
Erhimuebru <erhimu.ebru@decagon.dev>
2023-02-11 19:15:21 +0100

    new

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(8000);
 }
 bootstrap();
